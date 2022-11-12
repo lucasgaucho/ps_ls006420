7b6250c652d61b0d83aa2121933d449d8ca2f0c6
Lucas HS <ls006420@cfjl.com.br>
2022-11-11 21:15:55 -0300

    METODO RENDER

--- src/View/Render.php
+++ src/View/Render.php
@@ -2,6 +2,30 @@
 
 namespace PetShop\View;
 
+use Exception;
+
 class Render {
+    static public function front(string $pagina, array $dados) 
+    {
+        $pathPagina = TFRONTEND . 'pages/' . $pagina . 'php';
+
+        if(!file_exists($pathPagina)) {
+            error_log('Página template não localizada em: ' .'$pathPagina');
+            throw new Exception('A pagina solicitada não foi localizada');
+        }
+
+        if( empty($dados['titulo'])) {
+            $dados['titulo'] = FRONTEND_TITLE
+        } 
+        else {
+            $dados['titulo'] = $dados['titulo'] . ' - ' . FRONTEND_TITLE
+        }
+
+        extract($dados);
+    
+        require_once TPFRONTEND . 'common/top.php';
+        require_once $pathPagina;
+        require_once TPFRONTEND . 'common/bottom.php';
+    }
 
 }
