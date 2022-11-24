a9ea8959732afb7d24929cb663e32c15bb57da62
Lucas HS <ls006420@cfjl.com.br>
2022-11-24 17:21:16 -0300

    Rota criada

--- src/Core/App.php
+++ src/Core/App.php
@@ -10,10 +10,13 @@ class App
 
     public static function start()
     {
+        self::carregaSessao();
+
         self::$router = new Router;
 
         self::RegistraRotasdoBackEnd();
         self::RegistraRotasdoFrontEnd();
+        self::Registra404Generico();
         
         self::$router->run();
     }   
@@ -23,9 +26,24 @@ class App
         self::$router->get('/', '\Petshop\Controller\HomeController@index');
         self::$router->get('/login', '\Petshop\Controller\LoginController@login');
         self::$router->get('/cadastro', '\Petshop\Controller\CadastroController@cadastro');
+        self::$router->post('/cadastro', '\Petshop\Controller\CadastroController@postcadastro');
     }
     private static function RegistraRotasdoBackEnd()
     {
-        
+
+    }
+
+    public function registra404Generico()
+    {
+        self::$router->set404(function() {
+            header('HTTP/1.1 404 Not found');
+            $objErro = new ErrorController();
+            $objErro->erro404;
+        });
+    }
+
+    public function carregaSessao()
+    {
+        session_start();
     }
 }
