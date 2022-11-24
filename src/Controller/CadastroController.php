1beee414ed5ac0d24ca3434eda3670a03273c392
Lucas HS <ls006420@cfjl.com.br>
2022-11-24 16:56:43 -0300

    Tela de cadastro criada

--- src/Controller/CadastroController.php
+++ src/Controller/CadastroController.php
@@ -19,7 +19,24 @@ class CadastroController extends FrontController {
 
         public function FormCadastro()
         {
-            
+         $dados =[
+            'btn-label'=>'Criar minha conta',
+            'btn-class'=>'btn btn-sucess',
+            'fields'=>[
+                ['type'=>'radio-inline', 'class'=>'md-4', 'label'=>'Você é uma pessoa: ', 'name'=>'tipo', 'options'=>[
+                    ['label'=>'Fisica', 'value'=>'F'],
+                    ['label'=>'Juridica', 'value'=>'J']
+                    ], 'required'=>true
+                ],
+                ['type'=>'text', 'class'=>'col-6', 'label'=>'Documento', 'name'=>'cpfcnpj', 'required'=>true],
+                ['type'=>'text','label'=>'Seu nome completo', 'name'=>'nome', 'required'=>true],
+                ['type'=>'email','label'=>'Seu melhor e-mail', 'name'=>'email', 'required'=>true],
+                ['type'=>'password', 'class'=>'col-6', 'label'=>'Crie sua senha', 'name'=>'senha', 'required'=>true],
+                ['type'=>'password', 'class'=>'col-6', 'label'=>'Confirme sua senha', 'name'=>'senha2', 'required'=>true]
+            ]
+         ];
+
+         return Render::block('form', $dados);
         }
 
 }
